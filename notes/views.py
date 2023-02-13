c95092587795a45fc29bef962462b064eaa5c1e8
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-02-13 17:08:59 +0200

    start commit

--- notes/views.py
+++ notes/views.py
@@ -6,6 +6,6 @@ from django.http import HttpResponse
 def index(request):
     # Функція, що імплементує цей конкретний view
     # Щоби ця функція спрацювала, вона повинна бути прив'язана дор якогось запиту URL
-    return HttpResponse("Hello from Notes app and Serhii Miroshnychenko.")
+    return HttpResponse("Hello from <b>Notes app</b> and <b>Serhii Miroshnychenko</b>.")
     # HttpResponse -> функція, що повертає в браузер текстову відповідь,
     # передану їй як аргумент
